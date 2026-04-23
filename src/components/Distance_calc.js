b05d4456b9d63227aa06151ee6b46f66430a23bd
viktorwaller <viktor.waller@live.se>
2026-04-24 00:14:24 +0200

    Update Distance_calc.js

--- src/components/Distance_calc.js
+++ src/components/Distance_calc.js
@@ -1,5 +1,7 @@
 
-async function distanceClac(origin, destination){
+
+//Använd await innnan om du kallar denna funktion
+async function Calc_Distance(origin, destination){
     
     
     const API_KEY = "AIzaSyCjNsLzUbZz1D522m-rb9DnCSTkcKLuV_M";
@@ -10,7 +12,6 @@ async function distanceClac(origin, destination){
     const data = await response.json()
 
     
-
     //console.log("Google data: ", data)
     
     const distance = data.rows[0].elements[0].distance.value
@@ -20,10 +21,43 @@ async function distanceClac(origin, destination){
     return distance
 }
 
+
+async function Calc_Distance_Multi(origin, destinations) {
+    
+    const API_KEY = "AIzaSyCjNsLzUbZz1D522m-rb9DnCSTkcKLuV_M";
+
+    const destinationsApiFormat = destinations.join("|")
+
+    const URL = `https://maps.googleapis.com/maps/api/distancematrix/json?destinations=${destinationsApiFormat}&origins=${origin}&key=${API_KEY}`
+
+    const response = await fetch(URL)
+    const data = await response.json()
+
+    
+    
+
+    return data
+}
+
+
+
+
+
+
+
+
+
 const test_origin = "Lärdomsgatan 9, Göteborg"
 const test_destination = "Vasagatan 33, Göteborg"
+const tesst_dest2 = "Tubogatan 13, Norrköping"
+const distarray = [test_destination, tesst_dest2]
 
 
-const dist = await distanceClac(test_origin, test_destination)
+//const dist = await Calc_Distance(test_origin, test_destination)
+
+const dist = await Calc_Distance_Multi(test_origin, distarray)
 
 console.log("testing: ", dist)
+
+//https://console.cloud.google.com/marketplace/product/google/distance-matrix-backend.googleapis.com?q=search&referrer=search&project=project-03e05d93-b3c7-48de-bc7
+//https://developers.google.com/maps/documentation/distance-matrix/start#maps_http_distancematrix_start-txt
