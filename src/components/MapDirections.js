dce9e1d56b8a133003f404b7672244f52c2e8e9d
viktorwaller <viktor.waller@live.se>
2026-05-18 13:31:09 +0200

    klar med func

--- src/components/MapDirections.js
+++ src/components/MapDirections.js
@@ -2,10 +2,16 @@
 
 export function MapToFood(address){
     
-    const encAddress = encodeURIComponent(address)
+    try{ 
+        const encAddress = encodeURIComponent(address)
 
-    const googleMapsUrl = `https://www.google.com/maps/dir/?api=1&destination=${encAddress}`
+        const googleMapsUrl = `https://www.google.com/maps/dir/?api=1&destination=${encAddress}`
 
-    window.open(googleMaps, '_blank')
+        window.open(googleMaps, '_blank')
+    }
+    catch {
+        console.log("wrong address format for directions")
+    }
+    
 
 }
